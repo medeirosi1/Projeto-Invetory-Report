e7190a5ab07ad9f3d8a913cba696164c2073a1f4
Thiago Medeiros <medeiroshs@hotmail.com>
2022-10-28 15:16:14 -0300

    refactor: test product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -2,6 +2,17 @@ from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    teste = Product(1, "Ebytr", "Claudio", "2001-01-01", "2023-01-01", "123456", "em local seco")
-    mockString = "O produto Ebytr fabricado em 2001-01-01 por Claudio com validade até 2023-01-01 precisa ser armazenado em local seco."
+    teste = Product(
+        1,
+        "Ebytr",
+        "Claudio", "2001-01-01", "2023-01-01", "123456", "em local seco"
+    )
+    mockString = (
+        f'O produto {teste.nome_do_produto} '
+        f'fabricado em {teste.data_de_fabricacao} '
+        f'por {teste.nome_da_empresa} com validade '
+        f'até {teste.data_de_validade} '
+        f'precisa ser armazenado {teste.instrucoes_de_armazenamento}.'
+    )
+    print(mockString)
     assert teste.__repr__() == mockString
