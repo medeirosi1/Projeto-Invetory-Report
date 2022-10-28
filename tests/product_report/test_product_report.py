56ddb648d31047cc6182da4b9fe31187ba43f036
Thiago Medeiros <medeiroshs@hotmail.com>
2022-10-28 15:01:15 -0300

    feat: create test for product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,7 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    teste = Product(1, "Ebytr", "Claudio", "2001-01-01", "2023-01-01", "123456", "em local seco")
+    mockString = "O produto Ebytr fabricado em 2001-01-01 por Claudio com validade até 2023-01-01 precisa ser armazenado em local seco."
+    assert teste.__repr__() == mockString
