d684e6c559a0bd45c8dea9e88f7e235b2c9260ab
Thiago Medeiros <medeiroshs@hotmail.com>
2022-10-28 15:04:53 -0300

    refactor: test product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,13 +1,16 @@
 from inventory_report.inventory.product import Product
 
 
-
 def test_cria_produto():
-    teste = Product(1, "Ebytr", "Claudio", "2001-01-01", "2023-01-01", "123456", "em local seco")
-    assert teste.id == 1
-    assert teste.nome_do_produto == "Ebytr"
-    assert teste.nome_da_empresa == "Claudio"
-    assert teste.data_de_fabricacao == "2001-01-01"
-    assert teste.data_de_validade == "2023-01-01"
-    assert teste.numero_de_serie == "123456"
-    assert teste.instrucoes_de_armazenamento == "em local seco"
+    newProduct = Product(
+        1,
+        "Ebytr",
+        "Claudio", "2001-01-01", "2023-01-01", "123456", "em local seco"
+    )
+    assert newProduct.id == 1
+    assert newProduct.nome_do_produto == "Ebytr"
+    assert newProduct.nome_da_empresa == "Claudio"
+    assert newProduct.data_de_fabricacao == "2001-01-01"
+    assert newProduct.data_de_validade == "2023-01-01"
+    assert newProduct.numero_de_serie == "123456"
+    assert newProduct.instrucoes_de_armazenamento == "em local seco"
