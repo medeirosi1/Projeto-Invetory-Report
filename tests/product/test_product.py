94343372876781508c09f231a54d3da687ca9a0d
Thiago Medeiros <medeiroshs@hotmail.com>
2022-10-28 14:56:25 -0300

    feat: create test for product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,13 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    teste = Product(1, "Ebytr", "Claudio", "2001-01-01", "2023-01-01", "123456", "em local seco")
+    assert teste.id == 1
+    assert teste.nome_do_produto == "Ebytr"
+    assert teste.nome_da_empresa == "Claudio"
+    assert teste.data_de_fabricacao == "2001-01-01"
+    assert teste.data_de_validade == "2023-01-01"
+    assert teste.numero_de_serie == "123456"
+    assert teste.instrucoes_de_armazenamento == "em local seco"
