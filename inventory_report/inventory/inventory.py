d7f581b40dbc846f4aa63e6c7922c8039991f801
Thiago Medeiros <medeiroshs@hotmail.com>
2022-10-31 15:22:50 -0300

    feat: create intentory

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,28 @@
+import csv
+import json
+import xmltodict
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+
+    @classmethod
+    def import_data(cls, path, report):
+        data = cls.switch_path(path)
+        if report == "simples":
+            return SimpleReport.generate(data)
+        else:
+            return CompleteReport.generate(data)
+
+    @classmethod
+    def switch_path(cls, path):
+        if path.endswith('csv'):
+            with open(path) as csv_file:
+                return list(csv.DictReader(csv_file))
+        elif path.endswith('json'):
+            with open(path) as json_file:
+                return json.loads(json_file.read())
+        elif path.endswith('xml'):
+            with open(path) as xml_file:
+                return xmltodict.parse(xml_file.read())["dataset"]["record"]
